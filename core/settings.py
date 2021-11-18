41d7cc96672876f1b3428e6ff41d675b8d200093
Enda Kelly <kelly.enda@gmail.com>
2021-11-18 22:02:46 +0000

    added: mvc store front

--- core/settings.py
+++ core/settings.py
@@ -11,7 +11,9 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-from config import COINBASE_COMMERCE_API_KEY
+from core import config
+
+COINBASE_COMMERCE_API_KEY = config.COINBASE_COMMERCE_API_KEY
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
--- payments/views.py
+++ payments/views.py
@@ -2,5 +2,28 @@ from django.shortcuts import render
 
 # Create your views here.
 
+from coinbase_commerce.client import Client
+from django.shortcuts import render
+
+from core import settings
+
+
 def home_view(request):
-    return render(request, 'home.html', {})
+    client = Client(api_key=settings.COINBASE_COMMERCE_API_KEY)
+    domain_url = 'http://localhost:8000/'
+    product = {
+        'name': 'Whiskey',
+        'description': 'Sweet sweet nectar.',
+        'local_price': {
+            'amount': '65.00',
+            'currency': 'EUR'
+        },
+        'pricing_type': 'fixed_price',
+        'redirect_url': domain_url + 'success/',
+        'cancel_url': domain_url + 'cancel/',
+    }
+    charge = client.charge.create(**product)
+
+    return render(request, 'home.html', {
+        'charge': charge,
+    })
