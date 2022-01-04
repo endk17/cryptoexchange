31c96625e939a9c0738b90db4cdd9a27b275de04
Enda Kelly <kelly.enda@gmail.com>
2022-01-04 22:10:14 +0000

    redploy: local django config 3

--- app/core/settings.py
+++ app/core/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # 'DJANGO_ALLOWED_HOSTS' should be a single string of hosts with a space between each.
 # For example: 'DJANGO_ALLOWED_HOSTS=localhost 127.0.0.1 [::1]'
 # ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(" ")
-ALLOWED_HOSTS="localhost 127.0.0.1 [::1] .herokuapp.com"
+DJANGO_ALLOWED_HOSTS='localhost 127.0.0.1 [::1] .herokuapp.com fierce-retreat-10367.herokuapp.com'
 
 
 # Application definition
