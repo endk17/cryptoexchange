b9e1883b1dbbd0e9ee3661c49d2e50b82dc51fe1
Enda Kelly <38502722+endk17@users.noreply.github.com>
2022-01-04 16:32:39 +0000

    feature-heroku-deploy (#10)
    * added: custom github workflow for heroku
    
    * updated: formatting, imports and heroku deploy

--- app/payments/urls.py
+++ app/payments/urls.py
@@ -3,8 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home_view, name='payments-home'),
-    path('success/', views.success_view, name='payments-success'), 
-    path('cancel/', views.cancel_view, name='payments-cancel'),
-    path('webhook/', views.coinbase_webhook),
+    path("", views.home_view, name="payments-home"),
+    path("success/", views.success_view, name="payments-success"),
+    path("cancel/", views.cancel_view, name="payments-cancel"),
+    path("webhook/", views.coinbase_webhook),
 ]
