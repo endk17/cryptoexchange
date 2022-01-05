34184ee3735a3d749931f5b1a28e437ba5d2519d
Enda Kelly <kelly.enda@gmail.com>
2022-01-05 10:05:34 +0000

    fix: collectstatic set up for django & heroku

--- app/core/settings.py
+++ app/core/settings.py
@@ -16,7 +16,8 @@ COINBASE_COMMERCE_API_KEY =  os.environ.get("COINBASE_COMMERCE_API_KEY")
 COINBASE_COMMERCE_WEBHOOK_SHARED_SECRET = os.environ.get("COINBASE_COMMERCE_WEBHOOK_SHARED_SECRET")
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+# BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
@@ -83,9 +84,10 @@ WSGI_APPLICATION = "core.wsgi.application"
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        "NAME": os.path.join(BASE_DIR, "db.sqlite3")
+        # 'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
@@ -121,10 +123,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATIC_URL = "/static/"
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
-]
 
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
